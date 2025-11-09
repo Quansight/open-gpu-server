b77c54369672808283a75a9662a99216e1868269
Amit Kumar <dtu.amit@gmail.com>
2025-11-09 13:54:09 +0000

    Automate image build process

--- vm-images/build-image.sh
+++ vm-images/build-image.sh
@@ -7,28 +7,24 @@ fi
 
 set -ex
 
-DIB_RELEASE=noble
-DIB_CLOUD_IMAGES=https://cloud-images.ubuntu.com/noble/20251026/
-DIB_MODPROBE_BLACKLIST=”nouveau”
-DIB_CLOUD_INIT_DATASOURCES="OpenStack"
-DIB_DHCP_TIMEOUT=30
-DIB_NO_TMPFS=1
+export DIB_RELEASE=noble
+export DIB_CLOUD_IMAGES=https://cloud-images.ubuntu.com/noble/20251026/
+export DIB_MODPROBE_BLACKLIST="nouveau"
+export DIB_CLOUD_INIT_DATASOURCES="OpenStack"
+export DIB_DHCP_TIMEOUT=30
+export DIB_LOCAL_IMAGE=/opt/stack/.cache/image-create/noble-server-cloudimg-amd64.squashfs
+export DIB_NO_TMPFS=1
 ELEMENTS_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/elements"
 export ELEMENTS_PATH
 
-if [[ -z "${LOG_TO_FILE}" ]]; then 
-  LOG_TO_FILE="--logfile dib.log"
-fi
 if [[ -z "${IMAGE_YAML}" ]]; then 
   IMAGE_YAML="cpu-image.yaml"
-fi
-if [[ -z "${OUTPUT_IMAGE}" ]]; then 
-  output_fn=$(basename -- "${IMAGE_YAML}")
-  OUTPUT_IMAGE="${output_fn%.*}-$(date +%Y%m%d%H%M).qcow2"
 fi
 
 export TMP=$(pwd)
 echo "Starting Disk Image builder"
-sudo -E "$(which diskimage-builder)" "${IMAGE_YAML}" 
-mv "${IMAGE_YAML%.*}.qcow2" "${OUTPUT_IMAGE}"
-echo "Starting Disk Image Finished"
+echo "Using config: ${IMAGE_YAML}"
+
+sudo -E /opt/stack/miniconda3/envs/kolla/bin/diskimage-builder "${IMAGE_YAML}"
+
+echo "Disk Image Build Finished"
